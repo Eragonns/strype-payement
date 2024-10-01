26adcbbd3162293fbeb1f8749eb93d4db10ce2e5
Eragonns <lecomte.franck@outlook.com>
2024-10-01 16:28:01 +0200

    stripe

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import "express-async-errors";
+import "dotenv/config";
+import express from "express";
+import notFound from "./middlewares/not-found.middlewares.js";
+import errorHandler from "./middlewares/error-handler.middlewares.js";
+import { stripeController } from "./controllers/stripe.controller.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.static("public"));
+
+app.post("/create-payement-intent", stripeController);
+
+app.use(notFound);
+
+app.use(errorHandler);
+
+export default app;
